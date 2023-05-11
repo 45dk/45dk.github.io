ac4e4fb9dc8b06450f99b066132b780b200dc86b
Halit <ha.lit10@hotmail.com>
2023-05-11 17:52:25 +0300

    .

---
+++ db.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "sql200.epizy.com";
+$dbUsername = "epiz_34186577";
+$dbPassword = "0tmzvMyZsPW";
+$dbName = "games";
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+?>
